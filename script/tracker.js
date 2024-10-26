253ff9d2b5bfa177ccb67ba4654596ccaa02dfad
AvaTuttle <Travis.Tuttle@sdgku.edu>
2024-10-26 10:26:06 -0700

    first commit

---
+++ script/tracker.js
@@ -0,0 +1,22 @@
+
+
+function hello()
+{
+    console.log("hello there");
+}
+
+function extra()
+{
+    console.log("do you want fries with that")
+}
+
+
+function main()
+{
+    hello();
+    console.log("hello i am the main");
+    extra();
+} 
+
+
+window.onload = main;
